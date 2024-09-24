84b4754b16f865b6a20658f985d6ae81335d3e4a
h0203t <151034486+h0203t@users.noreply.github.com>
2024-09-24 22:33:52 +0900

    [Bronze III] Title: 알람 시계, Time: 184 ms, Memory: 18496 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 0 ~ 1439
-        int value = sc.nextInt()*60 + sc.nextInt() + 1440;
+        int value = sc.nextInt()*60 + sc.nextInt() + 1395;
         sc.close();
-        value -= 45;
-
         System.out.println((value/60)%24 + " " + value%60);
     }
 }
