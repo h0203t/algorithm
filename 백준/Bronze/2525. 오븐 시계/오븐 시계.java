5906a1b7f0243b2ec763416bc8e7bcb76c00dbaa
h0203t <151034486+h0203t@users.noreply.github.com>
2024-09-25 21:53:07 +0900

    [Bronze III] Title: 오븐 시계, Time: 188 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,23 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
-        int m = sc.nextInt();
-        int t = sc.nextInt();
-
-        h += t / 60;
-        m += t % 60;
-        
-        if(m >= 60){
-            h += 1;
-            m -= 60;
-        }
-        if(h >= 24){
-            h -= 24;
-        }
-        
-        System.out.println(h + " " + m);
+        Scanner scan = new Scanner(System.in);
+        int h=scan.nextInt(),m=scan.nextInt(),time=scan.nextInt();
+        h=(time+m)/60+h;
+        h%=24;
+        m=(time+m)%60;
+        System.out.print(h+" "+m);
     }
 }
